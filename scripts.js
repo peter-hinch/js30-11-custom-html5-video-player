46a0f6c4aa467c3edc992d17498d7faf6e946983
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-27 00:22:11 +1100

    Add skip forward / backward functionality.

--- scripts.js
+++ scripts.js
@@ -21,9 +21,16 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 /* Create event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));

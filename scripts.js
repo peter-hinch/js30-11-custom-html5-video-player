e2ce0ec09c7ed48033601e79961d995de208761f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-27 00:16:03 +1100

    Add pause icon to play / pause toggle.

--- scripts.js
+++ scripts.js
@@ -16,6 +16,14 @@ function togglePlay() {
   video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚❚';
+  toggle.textContent = icon;
+}
+
 /* Create event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
